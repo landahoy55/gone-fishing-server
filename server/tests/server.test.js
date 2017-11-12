2c44c53666044acff7bd58f414f50e5062782382
Pete <landahoy55@gmail.com>
2017-11-12 12:39:14 +0000

    Add tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -6,14 +6,25 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
-//this test method runds before each test. Currently we are dropping the db before each test
+
+//Seed data to add to the db. Allows us to add data, but also make it consistent.
+const todos = [{
+    text: "First test todo"
+}, {
+    text: "Second test todo"
+}];
+
+
+//this test method runds before each test. Currently we are dropping the db before each test and adding seed data
 beforeEach((done)=>{
-    Todo.remove({}).then( () => done());
+    Todo.remove({}).then( () => {
+        return Todo.insertMany(todos)
+    }).then(()=>done());
 })
 
 describe('POST /todos', () => {
     it('should create a new todo', (done) =>{
-        var text = "Test - Todo text"
+        var text = "testing test todo"
 
         //using supertest to post a todo. checking port and body
         request(app)
@@ -32,7 +43,7 @@ describe('POST /todos', () => {
                     return done(err);
                 }
 
-                Todo.find().then((todos)=>{
+                Todo.find({text}).then((todos)=>{
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done();
@@ -57,16 +68,26 @@ describe('POST /todos', () => {
                 } 
 
                 Todo.find().then((todos) => {
-                    expect(todos.length).toBe(0)
+                    expect(todos.length).toBe(2)
                     done();
                 }).catch((e) => done(e));
 
             });
-
     });
+});
 
 
+describe('GET /todos', () => {
+    
+    it('should get all todos', (done) => {
 
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todos.length).toBe(2);
+            }).end(done);
+        })
 });
 
 
